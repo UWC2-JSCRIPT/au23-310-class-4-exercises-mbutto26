6772a6796ef1b6d7ffe89fa45142f5fee362dcb7
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-10-31 18:53:37 -0700

    In class exercise

--- foodIsCooked.js
+++ foodIsCooked.js
@@ -1,20 +1,27 @@
 /**
  * Determines whether meat temperature is high enough
- * @param {string} kind 
- * @param {number} internalTemp 
+ * @param {string} kind
+ * @param {number} internalTemp
  * @param {string} doneness
  * @returns {boolean} isCooked
  */
-const foodIsCooked = function(kind, internalTemp, doneness) {
+const foodIsCooked = function (kind, internalTemp, doneness) {
   // Write function HERE
-
-}
-
-
+  if (kind === `chicken` && internalTemp > 165) {
+    return true;
+  } else if (kind === `beef` && doneness === rare && internalTemp > 125) {
+    return true;
+  } else if (kind === `beef` && doneness === medium && internalTemp > 135) {
+    return true;
+  } else if (kind === `beef` && doneness === well && internalTemp > 155) {
+    return true;
+  }
+  return false;
+};
 
 // Test function
-console.log(foodIsCooked('chicken', 90)); // should be false
-console.log(foodIsCooked('chicken', 190)); // should be true
-console.log(foodIsCooked('beef', 138, 'well')); // should be false
-console.log(foodIsCooked('beef', 138, 'medium')); // should be true
-console.log(foodIsCooked('beef', 138, 'rare')); // should be true
+console.log(foodIsCooked("chicken", 90)); // should be false
+console.log(foodIsCooked("chicken", 190)); // should be true
+console.log(foodIsCooked("beef", 138, "well")); // should be false
+console.log(foodIsCooked("beef", 138, "medium")); // should be true
+console.log(foodIsCooked("beef", 138, "rare")); // should be true
