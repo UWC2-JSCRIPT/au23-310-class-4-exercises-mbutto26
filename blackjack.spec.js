868b95e7e9a87c28357300ce79de06f031d2f2ad
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-11-02 16:53:26 -0700

    Names of players

--- blackjack.spec.js
+++ blackjack.spec.js
@@ -1,4 +1,10 @@
-const player1 = `player1`;
-const player2 = `dealer`;
+class cardPlayer {
+  constructor(name) {
+    this.name = name;
+  }
+}
+const player1 = new cardPlayer(`player1`);
+const player2 = new cardPlayer(`dealer`);
+
 console.log(player1);
-console.log(dealer);
+console.log(player2);
